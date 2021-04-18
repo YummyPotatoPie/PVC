7d78b1addbfb6aaa6fd327d03d33025931c69f40
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-04-18 16:15:28 +0300

    Added main class

---
+++ src/PVC/PVCRequestHandler.java
@@ -0,0 +1,12 @@
+package PVC;
+
+import PVC.Initializer.PVCInitializer;
+import PVC.CommitHandler.PVCCommitHandler;
+
+public class PVCRequestHandler {
+
+    public static void main(String[] args) {
+
+    }
+
+}
