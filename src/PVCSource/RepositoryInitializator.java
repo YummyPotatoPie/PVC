03df66b3e7180c6a861c40c56b051bdf1a5d0d52
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-04-19 14:47:28 +0300

    Some changes (I'm forgot what I'm changed)

--- src/PVCSource/RepositoryInitializator.java
+++ src/PVCSource/RepositoryInitializator.java
@@ -3,16 +3,23 @@ package PVCSource;
 import java.io.File;
 import java.io.IOException;
 
-class RepositoryInitializator implements Handler<String> {
+public class RepositoryInitializator implements Handler<String> {
+
+    private final String pvcMainFolderName = "\\.pvc";
+    private final String pvcCommitsFolder = "\\commits";
 
     public void handle(String path) {
 
-        boolean isSuccesfullyCreated = new File(path + "\\.pvc").mkdir();
+        boolean isSuccesfullyCreated = new File(path + this.pvcMainFolderName).mkdir();
 
-        if (isSuccesfullyCreated)
-            setHiddenAttribute(new File(path + "\\.pvc"));
-        else
+        if (isSuccesfullyCreated) {
+            setHiddenAttribute(new File(path + this.pvcMainFolderName));
+            createFolderSystem(path + this.pvcMainFolderName);
+        }
+        else {
             System.out.println("Something went wrong");
+            System.exit(1);
+        }
 
     }
 
@@ -23,6 +30,23 @@ class RepositoryInitializator implements Handler<String> {
         }
         catch (IOException | InterruptedException e) {
             System.out.println("Something went wrong");
+        }
+    }
+
+    private void createFolderSystem(String path) {
+        new File(path + this.pvcCommitsFolder).mkdir();
+
+        for (int i = 17; i < 256; i++) {
+            if (new File(path + this.pvcCommitsFolder + "\\" + Integer.toHexString(i)).mkdir())
+                continue;
+            else {
+                System.out.println("Something went wrong");
+
+                File pvcMainFolder = new File(path);
+                pvcMainFolder.delete();
+
+                System.exit(1);
+            }
         }
     }
 
