cb5a46da85dcaa968b47c322824bbd3762f6a4ec
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-04-19 13:22:18 +0300

    Added field

--- src/PVCSource/Commands.java
+++ src/PVCSource/Commands.java
@@ -8,10 +8,11 @@ class Commands {
     private static Options pvcOptions = new Options();
 
     public final static String initializationOptName = "init";
+    public final static String initializationOptDesc = "Repository initialization";
     private static Option initialization = Option.builder()
             .longOpt(initializationOptName)
             .hasArg(false)
-            .desc("Repository initialization")
+            .desc(initializationOptDesc)
             .build();
 
     public static Options getPVCOptions() {
