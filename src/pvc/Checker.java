38ad68bd76168b2e4da1de56d5e25d597ead4d72
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-04-22 01:32:54 +0300

    Class with must have methods

---
+++ src/pvc/Checker.java
@@ -0,0 +1,11 @@
+package pvc;
+
+import java.io.File;
+
+public class Checker {
+
+    public static boolean isRepositoryExist() {
+        return new File(System.getProperty("user.dir") + RepositoryInitializer.pvcMainFolderName).exists();
+    }
+
+}
