ee12f7850c01684f2dfc9b1f27b13611e7958387
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-04-24 07:32:50 +0300

    Added class which contains paths and special tokens

---
+++ src/pvc/PathsAndTokens.java
@@ -0,0 +1,17 @@
+package pvc;
+
+public class PathsAndTokens {
+
+    public final static String pvcMainFolderName = "\\.pvc";
+    public final static String pvcCommitsFolder = "\\commits";
+    public final static String pvcBranchesFolder = "\\branches";
+    public final static String pvcConfigFileName = "config";
+    public final static String pvcHEADFileName = "HEAD";
+    public final static String pvcAddFile = "add";
+    public final static String defaultBranchName = "main";
+    public final static String nullString = "null";
+    public final static String zeroCommitString = "0";
+    public final static String fileStart = "_FILE_START";
+    public final static String fileEnd = "_FILE_END";
+
+}
