5b4552e83660437ef34f9624a78f8e1acbeea29d
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-04-24 07:29:37 +0300

    Added new method

--- src/pvc/Checker.java
+++ src/pvc/Checker.java
@@ -2,14 +2,20 @@ package pvc;
 
 import java.io.File;
 
+import static pvc.PathsAndTokens.*;
+
 public class Checker {
 
     public static boolean isRepositoryExist() {
-        return new File(System.getProperty("user.dir") + RepositoryInitializer.pvcMainFolderName).exists();
+        return new File(System.getProperty("user.dir") + pvcMainFolderName).exists();
     }
 
     public static boolean isFolderExist(String path) {
         return new File(path).exists();
+    }
+
+    public static boolean isAddFileEmpty() {
+        return new File(System.getProperty("user.dir") + "\\" + pvcAddFile).length() == 0;
     }
 
 }
