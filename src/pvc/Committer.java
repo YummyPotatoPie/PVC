d4e541c910e49ecfa73ef7ba51beb3f857d3168a
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-04-30 19:59:17 +0300

    Added 3 methods

--- src/pvc/Committer.java
+++ src/pvc/Committer.java
@@ -1,35 +1,46 @@
 package pvc;
 
+import pvc.Exceptions.NothingToCommitError;
 import pvc.Exceptions.PVCException;
 import pvc.Exceptions.ProcessExecutionError;
 import pvc.Exceptions.RepositoryDoesNotExist;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 import static pvc.PathsAndTokens.*;
 
 public class Committer implements Handler<String> {
 
-    public void handle(String message) throws PVCException  {
+    public void handle(String message) throws PVCException {
 
-        if (!Checker.isRepositoryExist())
+        if (!Utilites.isRepositoryExist())
             throw new RepositoryDoesNotExist();
+
+        if (Utilites.isAddFileEmpty())
+            throw new NothingToCommitError();
 
         String commitHash = getCommitHash();
         String path = System.getProperty("user.dir") + pvcMainFolderName + pvcCommitsFolder;
 
-        if (Checker.isFolderExist(commitHash.substring(0, 2)))
+        if (Utilites.isFolderExist(commitHash.substring(0, 2)))
             createCommitFile(path + "\\" + commitHash.substring(0, 2) + "\\" + commitHash);
         else {
             createCommitFolder(path + "\\" + commitHash.substring(0, 2));
             createCommitFile(path + "\\" + commitHash.substring(0, 2) + "\\" + commitHash);
         }
+
+        addedFilesToBinaryRepesentation(commitHash.substring(0, 2), commitHash, message);
+        addFileFlush();
+        addCommitDataToBranchFile(Utilites.currentBranch(), commitHash, Integer.toString(Utilites.currentCommitID() + 1));
+        Utilites.HEADRewrite(Utilites.currentBranch(), commitHash, Integer.toString(Utilites.currentCommitID() + 1));
+
     }
 
     private String getCommitHash() throws ProcessExecutionError {
@@ -38,7 +49,8 @@ public class Committer implements Handler<String> {
             String currentTime = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
             commitHash.update(currentTime.getBytes());
 
-            return String.format("%64x", new BigInteger(1, commitHash.digest()));
+            String result = String.format("%64x", new BigInteger(1, commitHash.digest()));
+            return result.charAt(0) == ' ' ? result.substring(1) : result;
         }
         catch (NoSuchAlgorithmException e) {
             throw new ProcessExecutionError();
@@ -64,8 +76,60 @@ public class Committer implements Handler<String> {
         }
     }
 
-    private void addedFilesToBinaryRepesentation() {
+    private void addedFilesToBinaryRepesentation(String commitFolderPath, String commitFileHash, String message) throws ProcessExecutionError {
+        String path = System.getProperty("user.dir") + pvcMainFolderName;
+        File commitInput = new File(path + pvcCommitsFolder + "\\" + commitFolderPath + "\\" + commitFileHash);
+        File addFile = new File(path + "\\" + pvcAddFile);
 
+        try {
+            Scanner scanner = new Scanner(addFile);
+            OutputStream binaryStream = new FileOutputStream(commitInput);
+            binaryStream.write((message + PathsAndTokens.message).getBytes());
+
+            while (scanner.hasNextLine()) {
+                String filename = scanner.nextLine();
+                File addedFilePath = new File(System.getProperty("user.dir") + "\\" + filename);
+                Scanner addedFileScanner = new Scanner(addedFilePath);
+
+                binaryStream.write((filename + fileStart).getBytes());
+
+                while (addedFileScanner.hasNextLine())
+                    binaryStream.write(addedFileScanner.nextLine().getBytes());
+
+                binaryStream.write(("\n" + filename + fileEnd).getBytes());
+                addedFileScanner.close();
+            }
+            binaryStream.close();
+        }
+        catch (IOException | NoSuchElementException e) {
+            throw new ProcessExecutionError();
+        }
+
+    }
+
+    private void addFileFlush() throws ProcessExecutionError {
+        String path = System.getProperty("user.dir") + pvcMainFolderName + "\\" + pvcAddFile;
+        File file = new File(path);
+        try {
+            PrintWriter writer = new PrintWriter(file);
+            writer.print("");
+            writer.close();
+        }
+        catch (FileNotFoundException e) {
+            throw new ProcessExecutionError();
+        }
+    }
+
+    private void addCommitDataToBranchFile(String branchName, String commitHash, String commitID) throws ProcessExecutionError {
+        File branchFile = new File(System.getProperty("user.dir") + pvcMainFolderName + pvcBranchesFolder + "\\" + branchName);
+
+        try {
+            FileWriter writer = new FileWriter(branchFile);
+            writer.write(commitHash + " " + commitID);
+            writer.close();
+        } catch (IOException e) {
+            throw new ProcessExecutionError();
+        }
     }
 
 }
