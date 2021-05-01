66f2ac5da133b5362734bacc3c0289c6e2f85ee5
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-05-01 22:04:40 +0300

    Merge command class

---
+++ src/pvc/Merger.java
@@ -0,0 +1,94 @@
+package pvc;
+
+import pvc.Exceptions.*;
+
+import static pvc.PathsAndTokens.pvcBranchesFolder;
+import static pvc.PathsAndTokens.pvcMainFolderName;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Merger implements Handler<String> {
+
+    public void handle(String branchName) throws PVCException {
+
+        if (!Utilites.isRepositoryExist())
+            throw new RepositoryDoesNotExist();
+
+        if (!Utilites.isBranchExist(branchName))
+            throw new BranchDoesNotExistError();
+
+        if (!isCompatibleCommitHistory(branchName))
+            throw new CommitHistoryIsNotCompatibleError();
+
+        mergeBranches(branchName);
+
+    }
+
+    private boolean isCompatibleCommitHistory(String branchName) throws ProcessExecutionError, HEADFileCorruptedError {
+        File currentBranch = new File(System.getProperty("user.dir") + pvcMainFolderName +
+                pvcBranchesFolder + "\\" + Utilites.currentBranch());
+
+        File argBranch = new File(System.getProperty("user.dir") + pvcMainFolderName +
+                pvcBranchesFolder + "\\" + branchName);
+
+        try {
+            Scanner currentBranchReader = new Scanner(currentBranch);
+            Scanner argBranchReader = new Scanner(argBranch);
+
+            while (currentBranchReader.hasNextLine() && argBranchReader.hasNextLine()) {
+                if (!currentBranchReader.nextLine().equals(argBranchReader.nextLine())) {
+                    return false;
+                }
+            }
+
+            if (!currentBranchReader.hasNextLine()) {
+                currentBranchReader.close();
+                argBranchReader.close();
+                return true;
+            }
+            else {
+                currentBranchReader.close();
+                argBranchReader.close();
+                return false;
+            }
+
+        }
+        catch (IOException ioe) {
+            throw new ProcessExecutionError();
+        }
+
+    }
+
+    private void mergeBranches(String branchName) throws ProcessExecutionError, HEADFileCorruptedError {
+        try {
+            File currentBranch = new File(System.getProperty("user.dir") + pvcMainFolderName +
+                    pvcBranchesFolder + "\\" + Utilites.currentBranch());
+
+            File argBranch = new File(System.getProperty("user.dir") + pvcMainFolderName +
+                    pvcBranchesFolder + "\\" + branchName);
+
+            Scanner argBranchReader = new Scanner(argBranch);
+            Scanner currentBranchReader = new Scanner(currentBranch);
+            FileWriter currentBranchWriter = new FileWriter(currentBranch);
+
+            while (argBranchReader.hasNextLine()) {
+                String commitData = argBranchReader.nextLine();
+                if (!currentBranchReader.hasNextLine() || !currentBranchReader.nextLine().equals(commitData)) {
+                    currentBranchWriter.append(commitData).append("\n");
+                }
+            }
+
+            argBranchReader.close();
+            currentBranchReader.close();
+            currentBranchWriter.close();
+
+        }
+        catch (IOException ioe) {
+            throw new ProcessExecutionError();
+        }
+    }
+
+}
