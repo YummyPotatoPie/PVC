e2356afa8cfb2b933cb4e9d4499301ab404e1f91
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-05-03 02:21:48 +0300

    WORKING LAUNCHER OMG

--- launcher/pvclauncher.cpp
+++ launcher/pvclauncher.cpp
@@ -1,6 +1,7 @@
 ﻿#include <windows.h>
 #include <shellapi.h>
 #include <string>
+#include <iostream>
 
 std::string argsToString(int argc, char* argv[]) {
     std::string result = "";
@@ -15,11 +16,36 @@ std::string argsToString(int argc, char* argv[]) {
 
 int main(int argc, char* argv[]) {
 
+    char path[MAX_PATH];
+    GetModuleFileNameA(NULL, path, MAX_PATH);
+    std::string pathToPVCJar = std::string(path);
+    pathToPVCJar = pathToPVCJar.substr(0, pathToPVCJar.size() - 8);
+
     std::string pvcArgsString = argsToString(argc, argv);
-    std::wstring pvcJarExecute = L"/K java -jar pvc.jar ";
+    std::wstring pvcJarPath = std::wstring(pathToPVCJar.begin(), pathToPVCJar.end()) + L"\\pvc.jar ";
+    std::wstring pvcJarExecute = L"java -jar " + pvcJarPath;
     std::wstring pvcArgsWstring = std::wstring(pvcArgsString.begin(), pvcArgsString.end());
     std::wstring executeCommand = pvcJarExecute + pvcArgsWstring;
 
-    ShellExecute(NULL, L"open", L"cmd.exe", executeCommand.c_str() , NULL, SW_HIDE);
+    std::wcout << executeCommand << std::endl;
+
+    //Deprecated
+    //ShellExecute(NULL, L"open", L"cmd.exe", executeCommand.c_str() , NULL, SW_HIDE);
+    
+    PROCESS_INFORMATION ProcessInfo;
+    STARTUPINFO StartUpInfo;
+
+    ZeroMemory(&StartUpInfo, sizeof(StartUpInfo));
+    StartUpInfo.cb = sizeof StartUpInfo; 
+
+    if (CreateProcess(NULL, LPWSTR(executeCommand.c_str()), NULL, NULL, FALSE, 0, NULL, NULL, &StartUpInfo, &ProcessInfo)) {
+        WaitForSingleObject(ProcessInfo.hProcess, INFINITE);
+        CloseHandle(ProcessInfo.hThread);
+        CloseHandle(ProcessInfo.hProcess);
+    }
+    else {
+        std::cout << "pvc cannot be executed" << std::endl;
+    }
+    
     return 0;
 }
